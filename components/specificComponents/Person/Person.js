89b31e6c0efae146645893d6f4a3d36cf19c343a
RemiDekok <remi.decock@ugent.be>
2024-11-30 18:08:41 +0100

    kwn

--- components/specificComponents/Person/Person.js
+++ components/specificComponents/Person/Person.js
@@ -14,12 +14,12 @@ export default class Person extends Component {
 				<div {...storyblokEditable(this.props.blok)} className={css["wrapper"]}>
 					<div className={css["content"]}>
 						<div className={[css["box"], css["head"]].join(" ")}>
-							<h1>{this.props.blok.title} {this.props.blok.lastname} {this.props.blok.firstname}</h1>
+							<h1>{this.props.blok.title}</h1>
 						</div>
 						<div className={[css["box"], css["sidebar"]].join(" ")}>
 							<div className={css["personalimage"]}><img src={this.props.blok.photo.filename} /></div>
 							<div className={css["personaldetails"]}>
-								<div className={css["personaldetailitem"]}>{this.props.blok.title} {this.props.blok.firstname} {this.props.blok.lastname}</div>
+								<div className={css["personaldetailitem"]}>{this.props.blok.title}</div>
 								<div className={css["personaldetailitem"]}>{this.props.blok.dateofbirth}</div>
 								<div className={css["personaldetailitem"]}>{this.props.blok.location}</div>
 							</div>
