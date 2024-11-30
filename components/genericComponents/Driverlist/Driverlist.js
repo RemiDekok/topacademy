cc7156cfa24a427000abb07c5d8c64427051395a
RemiDekok <remi.decock@ugent.be>
2024-11-30 22:01:00 +0100

    story

--- components/genericComponents/Driverlist/Driverlist.js
+++ components/genericComponents/Driverlist/Driverlist.js
@@ -15,7 +15,7 @@ export default class List extends Component {
         <section className={css["rich-text-section"]} {...storyblokEditable(this.props.blok)}>
           <h2 className={css["rich-text-section__title"]}>{this.props.blok.title}</h2>
           {this.props.blok.elements.map((nestedBlok) => (
-            <Element blok={nestedBlok} key={nestedBlok._uid} />
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
           ))}
         </section>
       </>
