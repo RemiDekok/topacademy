ed5ebb2a9e1551e47cdbd0bb0f71afcc25ca2737
RemiDekok <remi.decock@ugent.be>
2024-11-30 14:34:27 +0100

    list try

--- components/genericComponents/List/List.js
+++ components/genericComponents/List/List.js
@@ -1,24 +1,40 @@
 import { Component } from "react";
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { storyblokEditable } from "@storyblok/react";
 import Element from "../Element/Element";
 import css from "./List.module.scss";
 
 export default class List extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-
   render() {
+    const { blok } = this.props;
+
     return (
-      <>
-        <section className={css["rich-text-section"]} {...storyblokEditable(this.props.blok)}>
-          <h2 className={css["rich-text-section__title"]}>{this.props.blok.title}</h2>
-          {this.props.blok.elements.map((nestedBlok) => (
-            <Element blok={nestedBlok} key={nestedBlok._uid} />
+      <section className={css["rich-text-section"]} {...storyblokEditable(blok)}>
+        {/* Titel bovenaan */}
+        <h2 className={css["rich-text-section__title"]}>{blok.title}</h2>
+        
+        <div className={css["rich-text-section__list"]}>
+          {blok.elements.map((nestedBlok) => (
+            <div className={css["rich-text-section__item"]} key={nestedBlok._uid}>
+              {/* Afbeelding */}
+              {nestedBlok.image && (
+                <img
+                  src={nestedBlok.image.filename}
+                  alt={nestedBlok.image.alt || nestedBlok.title}
+                  className={css["rich-text-section__image"]}
+                />
+              )}
+              {/* Titel */}
+              <h3 className={css["rich-text-section__item-title"]}>
+                {nestedBlok.title}
+              </h3>
+              {/* Subtitel */}
+              <p className={css["rich-text-section__subtitle"]}>
+                {nestedBlok.subtitle}
+              </p>
+            </div>
           ))}
-        </section>
-      </>
+        </div>
+      </section>
     );
   }
 }
