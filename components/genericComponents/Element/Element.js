a9771248cf9cff0bea64bb1e155cd5f6243e2219
RemiDekok <remi.decock@ugent.be>
2024-11-30 21:53:02 +0100

    chatgpt

--- components/genericComponents/Element/Element.js
+++ components/genericComponents/Element/Element.js
@@ -10,12 +10,16 @@ export default class Element extends Component {
 	}
 
 	render() {
+		const { blok } = this.props;
+		// Make sure blok.content and blok.content.title exist
+		const title = blok.content?.title || "No title available"; // Fallback title if missing
+
 		return (
 			<>
-				<div {...storyblokEditable(this.props.blok.content)} className={css["item"]}>
-					<Link className={css["link"]} href={`${this.props.blok.full_slug}`}>
+				<div {...storyblokEditable(blok.content)} className={css["item"]}>
+					<Link className={css["link"]} href={`${blok.full_slug}`}>
 						<div className={css["header"]}>
-							<span className={css["title"]}>yoo{this.props.blok.content.title}</span>
+							<span className={css["title"]}>{title}</span>
 						</div>
 					</Link>
 				</div>
