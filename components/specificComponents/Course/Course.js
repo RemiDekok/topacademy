a0a20099dbf6e7f21c54195cb977db87380ca059
RemiDekok <remi.decock@ugent.be>
2024-11-30 15:44:24 +0100

    course

--- components/specificComponents/Course/Course.js
+++ components/specificComponents/Course/Course.js
@@ -22,13 +22,13 @@ export default class Course extends Component {
 					<div className={css["course-page__main-content"]}>
 						<div id="course-page__short-description" key="course-page__short-description" className={css["course-page__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>What will we learn</h2>
+								<h2 className={css["rich-text-section__title"]}>About the team</h2>
 								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
 							</section>
 						</div>
 						<div id="course-page__short-description" key="course-page__short-description" className={css["course-page__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>Teachers</h2>
+								<h2 className={css["rich-text-section__title"]}>Team members</h2>
 								{this.props.blok.teachers && this.props.blok.teachers.map((teacher) => (
 									<TeacherCard blok={teacher} key={teacher._uid} />
 								))}
@@ -41,7 +41,7 @@ export default class Course extends Component {
 						</div>
 						<div id="course-page__short-description" key="course-page__short-description" className={css["course-page__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>Products</h2>
+								<p className={css["rich-text-section__title"]}>To be Continueed...</p>
 								{this.props.blok.products && this.props.blok.products.map((product) => (
 									<Element blok={product} key={product._uid} />
 								))}
