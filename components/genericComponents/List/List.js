db0435dcb9d3561439255eaa87fa2b202fbb3e1c
RemiDekok <remi.decock@ugent.be>
2024-11-30 14:53:22 +0100

    alles trg

--- components/genericComponents/List/List.js
+++ components/genericComponents/List/List.js
@@ -1,36 +1,24 @@
 import { Component } from "react";
-import { storyblokEditable } from "@storyblok/react";
-import css from "./List.module.scss"; // Import your styling
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import Element from "../Element/Element";
+import css from "./List.module.scss";
 
 export default class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
   render() {
-    const { blok } = this.props; // Get the blok (list) from props
-
-    return (
-      <section className={css["rich-text-section"]} {...storyblokEditable(blok)}>
-        {/* Title of the List */}
-        <h2 className={css["rich-text-section__title"]}>{blok.title}</h2>
-
-        <div className={css["rich-text-section__list"]}>
-          {/* Loop through each 'element' which is a Person block */}
-          {blok.elements.map((nestedBlok) => (
-            <div className={css["rich-text-section__item"]} key={nestedBlok._uid}>
-              
-              {/* Check if the image exists and display it */}
-              {nestedBlok.image && (
-                <img
-                  src={nestedBlok.image.filename} // Assuming the image field is called 'filename'
-                  alt={nestedBlok.image.alt || nestedBlok.title} // Fallback to title if no alt text
-                  className={css["rich-text-section__image"]}
-                />
-              )}
-              
-              {/* Display the title (name of the person) */}
-              <h3 className={css["rich-text-section__item-title"]}>{nestedBlok.title}</h3>
-            </div>
+    return (  
+      <>
+        <section className={css["rich-text-section"]} {...storyblokEditable(this.props.blok)}>
+          <h2 className={css["rich-text-section__title"]}>{this.props.blok.title}</h2>
+          {this.props.blok.elements.map((nestedBlok) => (
+            <Element blok={nestedBlok} key={nestedBlok._uid} />
           ))}
-        </div>
-      </section>
+        </section>
+      </>
     );
   }
 }
